09713fadd3603114682fd6756dafac67ec99e67c
ghk-sfeir <guidi.hk@sfeir.com>
2024-06-12 19:33:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + customer_name + '!', icon="✅")
 
 
-# New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
